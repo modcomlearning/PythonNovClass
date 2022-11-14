5eade2df5cfc295f418009850b30316a3ceabb68
modcomlearning <modcomlearning@gmail.com>
2022-11-14 20:02:12 +0300

    First Commit

--- test.py
+++ test.py
@@ -2,13 +2,14 @@
 from  Lesson6a import welcome, converter
 from  Lesson6b import converter2
 from  Lesson6c import interest
-# 6a, 6b, 6c are modules
 
+
+# 6a, 6b, 6c are modules
 welcome()
 converter()
 converter2(dollars=6000,rate=112)
-interest(p=80000, r = 12, t = 78)
-
+interest(r = 12, t = 78, p=80000)
+interest(6000, 7, 8)
 
 
 
